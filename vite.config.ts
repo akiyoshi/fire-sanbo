43f3d862d3c5d27b9eb74aa41a3120ac2c04ca0d
akiyoshi <23308120+akiyoshi@users.noreply.github.com>
2026-04-13 23:01:59 +0900

    chore: revert base path (Pages未設定のため)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/fire-sanbo/",
   plugins: [react(), tsconfigPaths()],
   worker: {
     format: "es",
